da91a1fa51df08c84759302cf1e68d4003cdc6da
rem <44992707+miurare@users.noreply.github.com>
2020-06-11 16:58:03 +0100

    bug fix

--- scriptForConvertUnits.py
+++ scriptForConvertUnits.py
@@ -5,7 +5,9 @@ def convertUnits(df):
         elif x == 'kg':
             return 1000
     
-    _tmp = df['重量'].str.extract('(?P<Number>\d*.?\d)(?P<Unit>[a-z]*)(?P<Upto>[ぁ-ん]*)')
-    df['重量'] = _tmp.Number.apply(float) * _tmp.Unit.apply(convertUnit)
+    _tmp = df['重量'].str.extract('(?P<Number>[0-9]*[.,]?[0-9]*)(?P<Unit>[a-z]*)(?P<Upto>[ぁ-ん]*)')
+    df['重量'] = _tmp.Number.str.replace(',','').apply(float) * _tmp.Unit.apply(convertUnitg)
+    _tmp = df['料金'].str.extract('(?P<Number>[0-9]*\,?[0-9]*)')
+    df['料金'] = _tmp.Number.str.replace(',','').apply(int)
     
     return df
